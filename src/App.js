8fa6cc8d30affb4434d676e7459cfae9293882b2
trip6love <lanebyers13@gmail.com>
2022-03-08 15:39:53 -0500

    finished about but need to fix bug for photo

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+// import "./App.css"; //
 
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />
+    }
+    if (currentPage === 'Portfolio') {
+      return <Projects />
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <About />
-      <Projects />
-      <Contact />
-    </main>
+    <div>
+      <Header />
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
   );
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,20 @@
-import React from "react";
+import pic from "../../assets/jaw.JPG";
 
-function about() {
+function About() {
     return (
-        
+        <div className="container-about">
+            <h1 className="about-header">About Me</h1>
+                <div className="about">
+                    <img 
+                        alt="Error Profile Picture" 
+                        src={pic} className="picture"
+                    ></img>
+            <p className="about-text">Hello! My name is Lane Byers and I am currently enrolled at the UNC Coding Bootcamp. 
+            This portfolio was created with a new skill; REACT! I want to continue my passions as a web developer, specifically front end.
+            Currently I live in Raleigh, North Carolina. Here I enjoy skateboarding, and being outdoors with the friends close to me!</p>
+        </div>
+        </div>
     )
 }
 
-export default about
+export default About
