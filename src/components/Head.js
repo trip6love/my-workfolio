76795201437c4a6af61c57f950a9e81f3d4eeb0a
trip6love <lanebyers13@gmail.com>
2022-03-08 16:07:47 -0500

    finished nav bar and head

---
+++ src/components/Head.js
@@ -0,0 +1,9 @@
+function head () {
+    return(
+        <header className="header">
+            <h1 className="head">Lane Byers</h1>
+        </header>
+    )
+}
+
+export default head;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,31 @@
 function navBar ({ currentPage, handlePageChange}) {
     return(
+        <ul className="nav">
+            <li>
+                <a href="#about"
+                    onClick={() => handlePageChange('About')}
+                    className={currentPage === 'About' ? 'nav-item-active' : 'nav-item'}
+                >
+                    About
+                </a>
+            </li>
+            <li>
+                <a href="#projects"
+                    onClick={() => handlePageChange('Projects')}
+                    className={currentPage === 'Projects' ? 'nav-item-active' : 'nav-item'}
+                >
+                    Projects
+                </a>
+            </li>
+            <li>
+                <a href="#contact"
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-item-active' : 'nav-item'}
+                >
+                    Contact
+                </a>
+            </li>
+        </ul>
 
     )
 }
