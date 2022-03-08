de195f7738cc30159dee950213ca78e42a51d9d6
trip6love <lanebyers13@gmail.com>
2022-03-08 14:37:54 -0500

    Starting the about component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+
 
 function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
