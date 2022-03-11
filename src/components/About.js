8fc95140506843c0fc59bef66e95353eb55a2e29
trip6love <lanebyers13@gmail.com>
2022-03-10 23:57:51 -0500

    added more styles

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,8 @@ function About() {
                         src={pic} className="picture"
                     ></img>
             <p className="about-text">Hello! My name is Lane Byers and I am currently enrolled at the UNC Coding Bootcamp. 
-            This portfolio was created with a new skill; REACT! I want to continue my passions as a web developer, specifically front end.
+            This portfolio was created with a new skill; REACT! I want to continue my passions as a web developer, specifically front end. For future work 
+            please feel free to contact me! 
             </p>
         </div>
         </div>
