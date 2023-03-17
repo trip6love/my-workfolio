83bbcf42cfce6642674a2a198569c9f756ac6434
trip6love <lanebyers13@gmail.com>
2023-03-17 18:00:43 -0400

    update

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,17 @@ import pic from "../assets/images/error.png";
 function About() {
     return (
         <div className="container-about">
-            <h1 className="about-header">About Me</h1>
+            <h1 className="about-header"></h1>
                 <div className="about">
-                    <img 
-                        alt="Error Profile" 
-                        src={pic} className="picture"
-                    ></img>
+                    
             <p className="about-text">Hello! My name is Lane Byers and I am currently a freelance website developer living in North Carolina. 
             You will be able to view current work of mine while I work on new projects. For any future graphic or web design work 
             please feel free to contact me at 919-621-6956 or lanebyers13@gmail.com.
             </p>
+                    <img 
+                        alt="Error Profile" 
+                        src={pic} className="picture"
+                    ></img>
         </div>
         </div>
     )
